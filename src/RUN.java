b3215670826511c5954bd261c10a778d600a9969
JadeLafleche <jade.lafleche@ocsbstudent.ca>
2017-04-10 14:40:31 -0400

    updatedd

---
+++ src/RUN.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class RUN {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in=new Scanner (System.in);
+		System.out.println("This program will tell you the first, middle, and last name you entered.");
+		String name,values[],x=0,avg,length; 
+			System.out.println("Enter a random name!");
+			
+	}
+		Arrays.@sort(values);
+		System.out.println(("The first name is "+values[values.length-1]));
+		
+		double avg=0;
+		for  (int x=0;x<values.length;x++){
+			double[] values;
+			avg=avg+values[x];
+		}
+		avg=avg/values.length;
+		System.out.format("Your average is %.2f", avg);
+
+
+	}
+
+}
